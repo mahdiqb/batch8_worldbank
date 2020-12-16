d7355cbf4f94840a2012087667ee78cd30570a54
mahdiqb <mahdikarabibene@yahoo.com>
2020-12-16 18:51:31 +0100

    add server

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -46,6 +46,8 @@ import dash_bootstrap_components as dbc
 
 external_stylesheets = ['assets/style.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
 
 # Import Geodata
 
