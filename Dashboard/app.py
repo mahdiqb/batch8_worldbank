29298d5f8d10ed48fc8ebd88f81538f1ea147e6b
mahdiqb <mahdikarabibene@yahoo.com>
2020-12-16 18:48:26 +0100

    remove unnecessary port

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -652,4 +652,4 @@ def callback(n_clicks, nh_clicks, style_map, style, btn1, btn2):
 # when file executed as a standalone application
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host="127.0.0.1", port=8050)
+    app.run_server(debug=False)
